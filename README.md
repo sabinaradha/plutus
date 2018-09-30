eee6943207a16f787e8247a11d0d438ec0363216
Sabina Koirala <sbnkrl@gmail.com>
2018-09-29 22:37:05 -0500

    Front App Created

--- README.md
+++
@@ -1,2 +0,0 @@
-# plutus
-## Bank App
---
+++ frontApp/src/main/java/com/bankapp/FrontAppApplication.java
@@ -0,0 +1,12 @@
+package com.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontAppApplication.class, args);
+	}
+}
